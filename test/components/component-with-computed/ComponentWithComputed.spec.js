b8b8e703d12dbb532ec8866a5cb62dfe8d5b33b2
soarflat <soarflat777@gmail.com>
2019-06-23 13:42:46 +0900

    test: computedを確認するテストを追加

---
+++ test/components/component-with-computed/ComponentWithComputed.spec.js
@@ -0,0 +1,23 @@
+import { shallowMount } from '@vue/test-utils';
+import NumberRenderer from '@/components/component-with-computed/ComponentWithComputed.vue';
+
+describe('NumberRenderer.vue', () => {
+  it('renders even numbers', () => {
+    const wrapper = shallowMount(NumberRenderer, {
+      propsData: {
+        even: true
+      }
+    });
+
+    expect(wrapper.text()).toBe('2, 4, 6, 8');
+  });
+
+  // call()を利用すればコンポーネントをレンダリングしなくてテストが可能
+  it('renders odd numbers', () => {
+    const localThis = { even: false };
+
+    expect(NumberRenderer.computed.numbers.call(localThis)).toBe(
+      '1, 3, 5, 7, 9'
+    );
+  });
+});
